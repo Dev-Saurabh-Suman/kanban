ee4181d4dcb4c47b6609df2801d5aaea6ea03059
unknown <ansh@capitalnumbers.com>
2022-08-26 21:21:17 +0530

    kanban view

--- init_db.py
+++ init_db.py
@@ -8,12 +8,24 @@ with open('schema.sql') as f:
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('First Post', 'Content for the first post')
+cur.execute("INSERT INTO posts (title, content, group_no) VALUES (?, ?, ?)",
+            ('First Post', 'Content for the first post', 1)
             )
 
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('Second Post', 'Content for the second post')
+cur.execute("INSERT INTO posts (title, content, group_no) VALUES (?, ?, ?)",
+            ('Second Post', 'Content for the second post', 2)
+            )
+
+cur.execute("INSERT INTO posts (title, content, group_no) VALUES (?, ?, ?)",
+            ('Third Post', 'Content for the third post', 1)
+            )
+
+cur.execute("INSERT INTO posts (title, content, group_no) VALUES (?, ?, ?)",
+            ('Fourth Post', 'Content for the fourth post', 3)
+            )
+
+cur.execute("INSERT INTO posts (title, content, group_no) VALUES (?, ?, ?)",
+            ('Fifth Post', 'Content for the fifth post', 4)
             )
 
 connection.commit()
